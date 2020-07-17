c6cff28447347e214f5ff7ca05e46849dee0ec63
ImmuneQQ <vitalyasatka@gmail.com>
2020-07-17 20:46:30 +0500

    new lessons

---
+++ React/Give Sibling Elements a Unique Key Attribute.js
@@ -0,0 +1,25 @@
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+  ];
+  
+  function Frameworks() {
+    const renderFrameworks = frontEndFrameworks.map(
+      (item) => {
+        return <li key={frontEndFrameworks.indexOf(item)}>{item}</li>
+      }
+    ); // change code here
+    return (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          {renderFrameworks}
+        </ul>
+      </div>
+    );
+  };
+  
